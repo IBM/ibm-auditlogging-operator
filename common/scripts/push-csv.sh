ff528f19eef725ded667548aee20ebcc54dee32f
Heather Bradfield <heather.bradfield@ibm.com>
2020-02-13 13:49:43 -0600

    fix quay repo and name

--- common/scripts/push-csv.sh
+++ common/scripts/push-csv.sh
@@ -17,8 +17,8 @@
 
 set -e
 QUAY_NAMESPACE=${QUAY_NAMESPACE:-opencloudio}
-QUAY_REPOSITORY=${QUAY_REPOSITORY:-ibm-metering-operator-app}
-BUNDLE_DIR=${BUNDLE_DIR:-deploy/olm-catalog/ibm-metering-operator}
+QUAY_REPOSITORY=${QUAY_REPOSITORY:-ibm-auditlogging-operator-app}
+BUNDLE_DIR=${BUNDLE_DIR:-deploy/olm-catalog/ibm-auditlogging-operator}
 
 [[ "X$QUAY_USERNAME" == "X" ]] && read -rp "Enter username quay.io: " QUAY_USERNAME
 [[ "X$QUAY_PASSWORD" == "X" ]] && read -rsp "Enter password quay.io: " QUAY_PASSWORD && echo
