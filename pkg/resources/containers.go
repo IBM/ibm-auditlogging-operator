39d5a17c01d8401c913b8cae0eb7fd70dc5facb3
Heather Bradfield <60789277+hbradfield@users.noreply.github.com>
2020-04-21 10:11:28 -0500

    create message key in http log records for qradar (#54)
    * fix missing http logs in qradar
    
    * add message key to http logs

--- pkg/resources/containers.go
+++ pkg/resources/containers.go
@@ -158,6 +158,14 @@ var sourceConfigData4 = `
         format json
         key_name message
         reserve_data true
+    </filter>
+    <filter icp-audit.*>
+        @type record_transformer
+        enable_ruby true
+        <record>
+          tag ${tag}
+          message ${record.to_json}
+        </record>
     </filter>`
 
 var splunkConfigData = `
