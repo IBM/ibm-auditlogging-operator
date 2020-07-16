88bac1f0043ba8d2b8ffa7de69e2c00e2732f911
Laurie Turner <21094172+lauriet-ibm@users.noreply.github.com>
2020-07-16 08:57:31 -0500

    update e2e link/add ingest cm/add ingest cm  to bindinfo (#98)
    * update e2e link
    
    * missing comma
    
    * review comments

--- pkg/resources/configmaps.go
+++ pkg/resources/configmaps.go
@@ -29,7 +29,11 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// EnableAuditLogForwardKey defines the key in the source config map for turning audit on or off
 const EnableAuditLogForwardKey = "ENABLE_AUDIT_LOGGING_FORWARDING"
+
+// HTTPIngestURLKey defines the Http endpoint
+const HTTPIngestURLKey = "AuditLoggingHttpIngestURL"
 
 // ConfigName defines the name of the config configmap
 const ConfigName = "config"
@@ -46,6 +50,9 @@ const QRadarConfigName = "remote-syslog-config"
 // SplunkConfigName defines the name of the splunk-hec-config configmap
 const SplunkConfigName = "splunk-hec-config"
 
+// HTTPIngestName defines the name of the http-ingesturl configmap
+const HTTPIngestName = "http-ingesturl"
+
 // FluentdConfigKey defines the key for the config configmap
 const FluentdConfigKey = "fluent.conf"
 
@@ -60,6 +67,8 @@ const QRadarConfigKey = "remoteSyslog.conf"
 
 // OutputPluginMatches defines the match tags for Splunk and QRadar outputs
 const OutputPluginMatches = "icp-audit icp-audit.**"
+
+const httpPath = "/icp-audit.http"
 
 var fluentdMainConfigData = `
 fluent.conf: |-
@@ -167,6 +176,7 @@ var FluentdConfigMaps = []string{
 	FluentdDaemonSetName + "-" + SourceConfigName,
 	FluentdDaemonSetName + "-" + SplunkConfigName,
 	FluentdDaemonSetName + "-" + QRadarConfigName,
+	FluentdDaemonSetName + "-" + HTTPIngestName,
 }
 
 // DataSplunk defines the struct for splunk-hec-config
@@ -229,6 +239,9 @@ func BuildConfigMap(instance *operatorv1alpha1.AuditLogging, name string) (*core
 			break
 		}
 		dataMap[QRadarConfigKey] = dq.Value
+	case FluentdDaemonSetName + "-" + HTTPIngestName:
+		p := strconv.Itoa(defaultHTTPPort)
+		dataMap[HTTPIngestURLKey] = "https://common-audit-logging" + "." + InstanceNamespace + ":" + p + httpPath
 	default:
 		reqLogger.Info("Unknown ConfigMap name")
 	}
